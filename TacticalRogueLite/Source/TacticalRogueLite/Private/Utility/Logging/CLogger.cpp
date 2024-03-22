e93cf2300d01f856244d5b9aaef0cee4b62f3670
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-03-22 12:07:31 +0100

    First commit -> Basic logging working

---
+++ TacticalRogueLite/Source/TacticalRogueLite/Private/Utility/Logging/CLogger.cpp
@@ -0,0 +1,60 @@
+﻿#include "Utility/Logging/CLogger.h"
+
+UCLogger* UCLogger::Instance = nullptr;
+
+UCLogger::UCLogger()
+{
+	Initialize();
+}
+
+UCLogger::~UCLogger()
+{
+	ShutDown();
+}
+
+UCLogger* UCLogger::Get()
+{
+	if (Instance == nullptr)
+	{
+		Instance = NewObject<UCLogger>();
+		Instance->AddToRoot(); // Prevent Garbage Collection
+	}
+	return Instance;
+}
+
+void UCLogger::Initialize()
+{
+	if(!bIsInitialized)
+	{
+		const FString Path = FPaths::ProjectLogDir() + TEXT("TacticalRogueLiteLogs/") + FDateTime::Now().ToString() + TEXT(".log");
+		LogFile = IFileManager::Get().CreateFileWriter(*Path);
+		if(LogFile)
+		{
+			bIsInitialized = true;
+			Log(TEXT("Logger Initialized"));
+			UE_LOG(LogTemp, Warning, TEXT("Logger Initialized with path: %s"), *Path);
+		}
+	}
+}
+
+void UCLogger::ShutDown()
+{
+	if(LogFile)
+	{
+		Log("Shutting Down");
+		LogFile->Close();
+		LogFile = nullptr;
+		bIsInitialized = false;
+	}
+}
+
+void UCLogger::Log(const FString Message)
+{
+	if(LogFile)
+	{
+		FString LogMessage = FDateTime::Now().ToString() + TEXT(" - ") + Message + LINE_TERMINATOR;
+		LogFile->Serialize(TCHAR_TO_ANSI(*LogMessage), LogMessage.Len());
+		LogFile->Flush();
+	}
+}
+
---
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/Utility/Logging/CLogger.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CLogger.generated.h"
+
+UCLASS(BlueprintType)
+class UCLogger : public UObject
+{
+	GENERATED_BODY()
+	UCLogger();
+	virtual ~UCLogger() override;
+	
+public:
+	static UCLogger* Get();
+
+	void Initialize();
+	void ShutDown();
+	
+	void Log(const FString Message);
+	
+private:
+	static UCLogger* Instance;
+
+	bool bIsInitialized = false;
+
+	FArchive* LogFile;
+};
