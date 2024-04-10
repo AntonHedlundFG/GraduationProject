13f47be1b636e981753f7c82e5273f61279b067e
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2024-04-10 14:47:10 +0200

    Added Main Menu button to ingame menu

--- TacticalRogueLite/Source/TacticalRogueLite/Private/GameMenuWidget.cpp
+++ TacticalRogueLite/Source/TacticalRogueLite/Private/GameMenuWidget.cpp
@@ -3,15 +3,40 @@
 
 #include "UI/GameMenuWidget.h"
 #include "Kismet/GameplayStatics.h"
+#include "GameFramework/GameModeBase.h"
+#include "TacticalRogueLite/OnlineSystem/Public/EpicOnlineSubsystem.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 void UGameMenuWidget::SetUp()
 {
 	Super::SetUp();
 	QuitButton->OnPressed.AddDynamic(this,&UGameMenuWidget::QuitGame);
+	MainMenuButton->OnPressed.AddDynamic(this, &UGameMenuWidget::QuitGame);
 }
 
 void UGameMenuWidget::QuitGame()
 {
+	auto* OnlineSystem = GetGameInstance()->GetSubsystem<UEpicOnlineSubsystem>();
+	if (OnlineSystem)
+		OnlineSystem->DestroySession();
+
 	UKismetSystemLibrary::QuitGame(GetWorld(),UGameplayStatics::GetPlayerController(GetWorld(),0),EQuitPreference::Quit, false);
+}
+
+void UGameMenuWidget::GoToMainMenu()
+{
+	auto* OnlineSystem = GetGameInstance()->GetSubsystem<UEpicOnlineSubsystem>();
+	if (OnlineSystem)
+		OnlineSystem->DestroySession();
+
+	AGameModeBase* GameMode = GetWorld()->GetAuthGameMode();
+	if (GameMode)
+	{
+		GameMode->ReturnToMainMenuHost();
+	}
+	else if (MainMenuRef)
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, MainMenuRef);
+	}
 }
--- TacticalRogueLite/Source/TacticalRogueLite/Public/UI/GameMenuWidget.h
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/UI/GameMenuWidget.h
@@ -15,8 +15,14 @@ class TACTICALROGUELITE_API UGameMenuWidget : public UCMovableWindow
 	GENERATED_BODY()
 
 	UPROPERTY(meta = (BindWidget))
+	UButton* MainMenuButton;
+	UPROPERTY(meta = (BindWidget))
 	UButton* QuitButton;
 	virtual void SetUp() override;
 	UFUNCTION()
 	void QuitGame();
+	UFUNCTION()
+	void GoToMainMenu();
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> MainMenuRef;
 };
