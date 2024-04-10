4d00a9ee7e823e97ec0eaba6161a3d02cb71148c
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-04-10 16:48:39 +0200

    Tags added to .h and .cpp

--- TacticalRogueLite/Source/TacticalRogueLite/Private/GamePlayTags/SharedGamePlayTags.cpp
+++ TacticalRogueLite/Source/TacticalRogueLite/Private/GamePlayTags/SharedGamePlayTags.cpp
@@ -11,6 +11,12 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_Action_Moving, "Action.Moving");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Action_DefaultAttack, "Action.DefaultAttack");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Action_Rooted, "Action.Rooted");
 
+UE_DEFINE_GAMEPLAY_TAG(TAG_Abilitytype, "Abilitytype");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Abilitytype_Offensive, "Abilitytype.Offensive");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Abilitytype_Defensive, "Abilitytype.Defensive");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Abilitytype_Supporting, "Abilitytype.Supporting");
+
+
 UE_DEFINE_GAMEPLAY_TAG(TAG_Movement, "Movement");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Movement_Straight, "Movement.Straight");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Movement_Diagonal, "Movement.Diagonal");
