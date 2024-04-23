788fd072463a2977308dca877542a2da84d79c53
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2024-04-23 15:35:19 +0200

    Comments

--- TacticalRogueLite/Source/TacticalRogueLite/Public/Actions/Visualizer/CActionVisualizerSystem.h
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/Actions/Visualizer/CActionVisualizerSystem.h
@@ -19,7 +19,9 @@ class UCAction;
 /// physically moving a pawn across the board, playing an animation, spawning a particle effect, 
 /// or even animating UI elements.
 /// 
-/// Each class of CAction is likely to have its own CActionVisualization sibling.
+/// Each class of CAction is likely to have its own CActionVisualization sibling, however it is possible
+/// to make "default" implementations, such as a generic projectile visualization for any projectile ability
+/// that has no specific visualization yet. Read the comment on AllAvailableVisualizationTemplates.
 /// 
 /// The idea is:
 /// CActions are managed by the server and replicated to each client.
@@ -47,24 +49,38 @@ protected:
 	UFUNCTION()
 	void OnActionListUpdate();
 
+	//Queue of visualizations that are created but have not yet been visualized.
 	TQueue<UCActionVisualization*> VisualizationQueue;
+	//Queue of past visualizations that are finished but whose actions have been undone.
 	TQueue<UCActionVisualization*> UndoQueue;
+
+	//Lookup map so that we can know which visualizations to undo when its action is undone.
 	TMap<UCAction*, UCActionVisualization*> ActionToVisualMap;
 
-	//Used to make sure they don't fall out of scope.
+	//We have to make sure visualization objects don't fall out of scope, so we keep them referenced here.
+	//If we need to clear the history for memory purposes, we can clean this array up at regular intervals.
 	UPROPERTY()
 	TArray<UCActionVisualization*> VisualizationList;
 
+	//This is the currently visualized object, which is retrieved from either the VisualizationQueue or UndoQueue
 	UCActionVisualization* CurrentVisualization;
+	//Are we visualizing this forwards, or backwards (do or undo)
 	bool bCurrentVisualForward;
 
+	//An index tracker so that we know which action to visualize, and maintain order.
 	int32 ActionListCurrentIndex = -1;
 	
-	//This should be populated in the editor so both C++ and BP implementations are available.
+	/// <summary>
+	/// This should be populated in the editor so both C++ and BP implementations are available.
+	/// It is a list of all available classes of ActionVisualizations. The order matters; 
+	/// as the first object that returns CanVisualizeAction() for an action will be chosen as the template.
+	/// This means a generic visualization should be at the end of the list, where a specific one should be at the top.
+	/// </summary>
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Actions")
 	TArray<TSubclassOf<UCActionVisualization>> AllAvailableVisualizationTemplates;
 
 	//Here we simply store actual UObject instances of each template, so we can call functions on it.
+	//The reason we don't use a static function call is that it isn't supported by AngelScript/Blueprints.
 	UPROPERTY()
 	TArray<UCActionVisualization*> AllAvailableVisualizationInstances;
 
