7cca91aca012098b352c1228545871e0cad6cee1
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-04-23 15:06:15 +0200

    SmoothDynamicsIntegrator component added for smooth dynamic interpolation..

---
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/Utility/SmoothDynamicsIntegrator.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SmoothDynamicsIntegrator.generated.h"
+
+UCLASS(BlueprintType)
+class USmoothDynamicsIntegrator : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	USmoothDynamicsIntegrator();
+	UFUNCTION(BlueprintCallable)
+	void Initialize(const FVector& InitialPosition);
+	UFUNCTION(BlueprintCallable)
+	FVector Update(float DeltaTime, const FVector& TargetPosition, FVector Velocity = FVector::ZeroVector);
+	
+	UPROPERTY(EditAnywhere)
+	float ResponseFrequency;
+	UPROPERTY(EditAnywhere)
+	float ResponseDamping;
+	UPROPERTY(EditAnywhere)
+	float ResponseUnderShoot;
+
+private:
+	FVector Previous;
+	FVector Current;
+	FVector Delta;
+	float K1, K2, K3;
+
+	FVector X0;
+};
