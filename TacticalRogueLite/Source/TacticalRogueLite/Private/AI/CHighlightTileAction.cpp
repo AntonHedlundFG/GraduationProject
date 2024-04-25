407bca65149076bc702305e9cc7b20988c7682cc
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-04-26 00:05:02 +0200

    First iteration of highlight ability and vis

---
+++ TacticalRogueLite/Source/TacticalRogueLite/Private/AI/CHighlightTileAction.cpp
@@ -0,0 +1,12 @@
+﻿#include "AI/CHighlightTileAction.h"
+#include "Grid/CGridTile.h"
+
+TArray<ACGridTile*> UCHighlightTileAction::GetValidTargetTiles_Implementation(ACGridTile* inTile)
+{
+	if(IsValid(HighlightedAbility))
+	{
+		return HighlightedAbility.GetValidTargetTiles(inTile);
+	}
+	return TArray<ACGridTile*>();
+}
+
---
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/Actions/Visualizer/CHighlightTilesVisualization.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/Visualizer/CActionVisualization.h"
+#include "CHighlightTilesVisualization.generated.h"
+
+class UCHighlightTileAction;
+class ACGridTile;
+
+UCLASS()
+class TACTICALROGUELITE_API UCHighlightTilesVisualization : public UCActionVisualization
+{
+	GENERATED_BODY()
+	
+	virtual bool CanVisualizeAction_Implementation(UCAction* Action) override;
+	
+	virtual void Enter_Implementation() override;
+
+	virtual bool Tick_Implementation(float DeltaTime) override;
+	virtual bool RevertTick_Implementation(float DeltaTime) override;
+
+	// Used to determine where in the animation we are.
+	float TimePassed = 0.0f;
+
+	// The total duration of the movement.
+	float Duration = 1.0f;
+
+	// A reference to the cast version of the action
+	UPROPERTY()
+	UCHighlightTileAction* HighLightAction;
+
+	// The tiles that are being highlighted
+	UPROPERTY()
+	TArray<ACGridTile*> HighlightedTiles;
+
+
+
+	
+};
