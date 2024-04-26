c3b04962f4cfa2a7f0a15ea4ca81d6f02cda8d2c
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-04-26 13:42:06 +0200

    Comments

--- TacticalRogueLite/Source/TacticalRogueLite/Public/AI/CAIController.h
+++ TacticalRogueLite/Source/TacticalRogueLite/Public/AI/CAIController.h
@@ -49,18 +49,37 @@ class TACTICALROGUELITE_API ACAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
-	UFUNCTION()
-	void OnTurnChanged();
+protected:
 	virtual void BeginPlay() override;
 
+	// Check if it is the AI's turn
+	// If it is, Execute its turn
+	UFUNCTION()
+	void OnTurnChanged();
+
+	// Score passed ability based on its considerations at the target tile
 	float ScoreAction(FAbility& Ability, ACGridTile* StartTile, ACGridTile* TargetTile);
+	
+	// Evaluate scored actions and decide which ones to use
 	FActionPath DecideBestActions();
+
+	// Loop through all abilities
+	// Recursively evaluate all possible paths
 	void EvalAbilitiesFromTile(ACGridTile* CurrentTile, TArray<FAbility> Abilities, TArray<FActionPath>& BestPaths, FActionPath& CurrentPath);
+
+	// Try to add the path to the best paths
 	void TryAddBestPath(FActionPath& NewPath, TArray<FActionPath>& inBestPaths);
+
+	// Execute the best path of abilities
 	void ExecuteActions(FActionPath& BestPath);
+
+	// Update the AI context
 	void UpdateContext();
+
+	// Execute the AI's turn
 	void ExecuteTurn();
+
+	// End the AI's turn
 	UFUNCTION()
 	void EndTurn();
 
